012cbf368b0882a72bfbcec2e758b886b94ce46c
Vincent Xue <xue.vin@gmail.com>
2018-03-26 00:34:41 +0000

    stockInputs

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import simplejson as json
+import numpy as np
+import pandas as pd
+import matplotlib
+
+
 
 app = Flask(__name__)
+
+
+def getPlot(ticker):
+   ticker='GOOG'
+   r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'/data.json?api_key=Eebg1xGXqxhS11D52xGs')
+   asJson = json.loads(r.content)
+   dataFormated = pd.DataFrame(np.array(asJson['dataset_data']['data']),columns=asJson['dataset_data']['column_names'])
+   asDateTime = pd.to_datetime(dataFormated.Date)
+
+   dates = matplotlib.dates.date2num(asDateTime.tolist())
+   values = np.array(dataFormated.Open).astype(float)
+
+   line = Line([dates,values],title="Cool")
+
+   return line
 
 @app.route('/')
 def index():
@@ -10,14 +32,20 @@ def index():
 def about():
   return render_template('about.html')
 
+app.vars = {}
 @app.route('/stock')
 def stock():
-   nquestions=5
-   return render_template('stock.html',num=nquestions)
+   return render_template('stock.html')
 
-@app.route('/index_lulu')
-def index_lulu()
-   ...
+@app.route('/stockVisualize',methods=['GET','POST'])
+def stockVisualize():
+   if request.method == 'GET':
+	return "oops"
+   else:
+      app.vars['stockTicker'] = request.form['ticker']
+      thisLine = getPlot(app.vars['stockTicker'])
+      #script, div = components(thisLine)
+      return render_template('stockVisualize.html',stockTicker=app.vars['stockTicker'])
 
 if __name__ == '__main__':
   app.run(port=33507)
